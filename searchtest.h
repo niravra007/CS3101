e7c6ff4c718ae26734e0d2d10a69afacb9211657
abhra-ray-math <ar22ms052@iiserkol.ac.in>
2024-11-17 15:14:29 +0530

    All files necessary to run program added
    NOTE: Student user files are not added. Sign in will not work unless you register at least 1 user.

---
+++ searchtest.h
@@ -0,0 +1,34 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+// Define the BOOK structure
+
+typedef struct book{
+    char title[255];
+    char id[50];
+    char author[50];
+    int no;
+    
+}BOOK;
+
+
+typedef struct bookuser{
+    char title[255];
+    char id[50];
+    char author[50];
+    char date[50];
+    
+}BOOKUSER;
+
+// Declare the fileRead function
+void fileSearch(char *filename, char *usertitle, char *userid, char *userauthor);
+void fileWrite(char *filename, BOOKUSER* books, int no_of_records);
+void fileReadUser(char *filename);
+void userDeleteEntry(char *filename, char *deleteID);
+void fileWriteBook(char *filename, BOOK* books, int no_of_records);
+void deleteBookEntry( char *deleteID);
+
+// Declare the search function prototype here if needed
+// void searchFunction(...);
+
+#endif // SEARCH_H
